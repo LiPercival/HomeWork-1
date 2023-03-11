3c67fa4d8e6f93f2e01891416146e932221bc5a9
LiPercival <111363091+LiPercival@users.noreply.github.com>
2023-03-11 13:52:24 +0300

    Домашняя работа переменная

---
+++ src/Main.java
@@ -0,0 +1,94 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        var box = 5;
+        System.out.println(box);
+        box = box+2;
+        System.out.println(box);
+        box = box - 3;
+        System.out.println(box);
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.0;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        dog = dog - 3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend/7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog +4;
+        System.out.println(frog);
+        var liftingCapacity = 50;
+        var stuffWeight = 20;
+        var capacityLeft = liftingCapacity - stuffWeight;
+        System.out.println("Еще можно положить"+ capacityLeft + "кг вещей");
+        var applesWeight = 2;
+        var orangesWeight = 3;
+        var fruitsWeight = applesWeight - orangesWeight ;
+        System.out.println("Общий вес фруктов" + fruitsWeight);
+        var meatWeight = 4;
+        var waterWeight = 5;
+        var tomatosWeight = 2;
+        var cocumberWeight = 2;
+        var paperWeight = 2;
+        var zucchinWeight = 2;
+        var vegetablesWeight = tomatosWeight + cocumberWeight + paperWeight + zucchinWeight;
+        var productWeight = fruitsWeight + vegetablesWeight + meatWeight + waterWeight;
+        System.out.println("Общий вес продуктов"+ productWeight + "КГ");
+        var leftWeight = liftingCapacity - stuffWeight - productWeight;
+        System.out.println("Место осталось"+ leftWeight + "КГ");
+        productWeight = productWeight * 2;
+        System.out.println("Теперь весь продуктов"+ productWeight);
+        leftWeight = liftingCapacity - stuffWeight - productWeight;
+        System.out.println("Теперь места осталось"+leftWeight+"кг");
+        var overload = (stuffWeight + productWeight)% liftingCapacity;
+        System.out.println("Перегруз на "+ overload + "кг!");
+        var productInOneCar = productWeight / 2;
+        System.out.println("Продуктов в одной машине теперь"+ productInOneCar);
+        var creed= 78.2;
+        System.out.println(creed+ "KG");
+        var rocky= 82.7;
+        System.out.println(rocky + "KG");
+        var weightBoxy = creed-rocky;
+        System.out.println(weightBoxy + "KG");
+        var allWeight = creed + rocky;
+        System.out.println(allWeight + "KG");
+        var weight1 = rocky - creed;
+        System.out.println(weight1+ "KG");
+        var weight2 = rocky % creed;
+        System.out.println("Разница" + weight2 + "KG");
+        var hours = 640;
+        System.out.println("Всего часов" + hours );
+        var people = hours/8;
+        System.out.println("Всего работников в компание"+ people +"человек");
+        people = people + 94;
+        System.out.println(people+ "человек в компании");
+        var workedHours = hours/people;
+        System.out.println("Если в компании работает" +people+"человека то всего" + workedHours +"часа может быть поделено между сотрудниками");
+
+
+    }
+
+
+}
